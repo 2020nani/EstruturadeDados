d02750c2d693e999274b9be863377774420efb72
hernani <megahack9@outlook.com>
2020-04-21 10:23:15 -0300

    algorithm ordered by the bubble process

---
+++ bolha.js
@@ -0,0 +1,19 @@
+//algoritmo de ordenacao de dados
+var valores = [7,6,5,4,3,2,1,0];
+function ordena(){
+  let inicio = 0;
+  let fim = 8;
+  let tmp;
+  for(vezes =0;vezes<8;vezes++){
+  for(pos = inicio;pos<fim-1-vezes;pos++){
+    if(valores[pos]>valores[pos+1]){
+      tmp = valores[pos];
+      valores[pos] = valores[pos+1];
+      valores[pos+1] = tmp
+    }
+  }
+}
+}
+ordena();
+console.log("Vetor ordenado ...");
+console.log(valores);
